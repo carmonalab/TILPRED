fcb70b97e2076c4ab63f2da83562097eec15a955
admin <sjcarmona@gmail.com>
2019-05-27 17:45:48 +0200

    return scAUCscore

--- R/predictTilState_function.R
+++ R/predictTilState_function.R
@@ -2,7 +2,7 @@
 #'
 #' \code{predictTilState} This function evaluates a logistic regression model to predict the state of individual CD8 tumor-infiltrating lymphocytes (mouse or human) based on their transcriptomes (scRNA-seq data)
 #'
-#' @param data single-cell expression object of class \emph{SingleCellExperiment} (if is.auc is FALSE) or matrix of enrichment scores (output of function scAUCscore, if is.auc is TRUE). For gene expression matrix, only gene expression ranks in each cell will be used and therefore any cell-to-cell normalization method used equivalent (as long as gene ranks are conserved among the top ~5"\%" genes). E.g. UMI counts, CPM, TPM, TMM are equivalent input types. Gene names correspond to mouse gene symbols (e.g. \emph{Cncb2}).
+#' @param data single-cell expression object of CD8 T-cells of class \emph{SingleCellExperiment} (if is.auc is FALSE) or matrix of enrichment scores (scAUCscore output, if is.auc is TRUE). For gene expression matrix, only gene expression ranks in each cell will be used and therefore any cell-to-cell normalization method used equivalent (as long as gene ranks are conserved among the top ~5"\%" genes). E.g. UMI counts, CPM, TPM, TMM are equivalent input types. Gene names correspond to mouse gene symbols (e.g. \emph{Cncb2}).
 #'
 #' @param is.auc logical value indicating if input matrix \emph{data} corresponds to single-cell expression matrix (FALSE, default) or matrix of enrichment scores, as calculated by scAUCscore function (TRUE)
 #'
@@ -12,8 +12,9 @@
 #'
 #' @return a two-element list containing 1) \emph{predictedState}, the predicted states
 #' (naive, effector, exhausted, memoryLike, or "unknown" if no class had a score above a threshold of \emph{scoreThreshold}),
-#' 2) \emph{stateProbabilityMatrix}, a matrix of number_of_cells x number_of_states (5) of probabilities of cell c belonging to class s,
-#' and 3) \emph{cycling}, logical vector indicating for each cell whethere there is a high cell cycle signal (independent to the cellular sub-type/state signal)
+#' 2) \emph{stateProbabilityMatrix}, a matrix of number_of_cells x number_of_states (4) of probabilities of cell c belonging to class s,
+#' 3) \emph{cycling}, logical vector indicating for each cell whethere there is a high cell cycle signal (independent to the cellular sub-type/state signal), and 4) \emph{scAUCscore} the matrix of AUC signature enrichment scores internally used for the prediction model (it can be used to re-run the prediction model)
+#'
 #'
 #' @examples
 ##' data(B16CD8TIL_SCE)
@@ -65,7 +66,7 @@ predictTilState <- function(data,nCores=1,is.auc=F,human=F,scoreThreshold=0.5) {
   cellClass[!cellClass %in% classNames]="unknown"
   cellClass=factor(cellClass,levels=c(classNames,"unknown"))
   names(cellClass)=rownames(probM)
-  return(list(predictedState=cellClass,stateProbabilityMatrix=probM,cycling=aucs["cycling",]>0.2))
+  return(list(predictedState=cellClass,stateProbabilityMatrix=probM,cycling=aucs["cycling",]>0.2,scAUCscore=aucs))
 
 }
 
